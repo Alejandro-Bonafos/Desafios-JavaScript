9976cadc8bdb7db4fd23b7eeabd0437d9b96b9d7
Alejandro Bonafos <alejandro.bonafos@gmail.com>
2022-09-06 17:54:03 -0300

    Js modificado

--- js/script.js
+++ js/script.js
@@ -8,23 +8,22 @@ let totalconcuotas = presupuesto
 const calculador = () => {
 
     if (cantidadCuotas != 1) {
-        for (i = 1; i <= cantidadCuotas; i++) {
-            let porCuota =((presupuesto * 2) / 100)
+      //  for (i = 1; i <= cantidadCuotas; i++) {
+            let porCuota =((presupuesto * 15) / 100)
             totalconcuotas = totalconcuotas + porCuota
-         }
+         
 
-        let valorPorCuota = (totalconcuotas / cantidadCuotas).toFixed(2)
-        alert ("Al acceder al plan de cuotas se te sumara un 2% en cada cuota. Deseas continuar?")
+        let valorPorCuota = (totalconcuotas / cantidadCuotas).toFixed(2) 
+        confirm ("Al acceder al plan de cuotas se te sumara un 2% en cada cuota. Deseas continuar?")
 
-        document.write( "Tu presupuesto es de  $" + presupuesto + " podemos ofrecerte los siguientes destinos: Tucuman - Cordoba - Corrientes. Abonando en " + cantidadCuotas + " el precio de cada cuota es de $" + valorPorCuota + " ya que cuenta con un recargo del 2% en cada cuota. Te Quedaria en un total de  $" + totalconcuotas)
+        document.write( "Tu presupuesto es de  $" + presupuesto + " podemos ofrecerte los siguientes destinos: Tucuman - Cordoba - Corrientes. Abonando en " + cantidadCuotas + " el precio de cada cuota es de $" + valorPorCuota + " ya que cuenta con un recargo del 15% en el total. Te Quedaria en un total de  $" + totalconcuotas)
     }
     
     else {
-        document.write( "Tu presupuesto es de $" + presupuesto + " y tenemos para ofrecerte estos destinos Tucuman - Cordoba - Corrientes. Pero ademas te contamos que accedes a un descuento del  20%  por realizar el pago en 1 cuota. Te quedaria un total de $" + (presupuesto - unpago) +  " Generaste un ahorro de $" +  unpago  + " FELICIDADES!")
-
+       document.write( "Tu presupuesto es de $" + presupuesto + " y tenemos para ofrecerte estos destinos Tucuman - Cordoba - Corrientes. Pero ademas te contamos que accedes a un descuento del  20%  por realizar el pago en 1 cuota. Te quedaria un total de $" + (presupuesto - unpago) +  " Generaste un ahorro de $" +  unpago  + " FELICIDADES!")
+    
     }
     
-
 }
 
 calculador()
